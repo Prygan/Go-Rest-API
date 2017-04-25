7210516c986814ec403b1d69b9cec38c6eb88ada
icyks <keovilay.k@hotmail.fr>
2017-04-25 08:59:22 +0200

    add example getOutings

--- main.go
+++ main.go
@@ -6,10 +6,23 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi")
-	})
+func getOutings(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		errorHandler(w, r, http.StatusNotFound)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Good Job to get Outings")
+}
 
+func main() {
+	http.HandleFunc("/Outings", getOutings)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
+	w.WriteHeader(status)
+	if status == http.StatusNotFound {
+		fmt.Fprint(w, "Error 404 not Found, don't hack plz")
+	}
 }
