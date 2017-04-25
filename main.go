ba2e156673e09c9375fee53085c51041fa857a6d
Prygan <dimitrisaingre@orange.fr>
2017-04-25 09:06:21 +0200

    add Outing structure

--- main.go
+++ main.go
@@ -1,15 +1,40 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/satori/go.uuid"
 )
 
+type outing struct {
+	ID     string `json:"id"`
+	Name   string `json:"name"`
+	Adress string `json:adress`
+	City   string `json:city`
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi")
-	})
+	http.HandleFunc("/", HelloWorld)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func newOuting(name string, adress string, city string) *outing {
+	return &outing{
+		ID:     uuid.NewV4().String(),
+		Name:   name,
+		Adress: adress,
+		City:   city,
+	}
+}
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	outing := newOuting("sortie ouf", "qq part", "nantes")
+
+	o, _ := json.Marshal(outing)
+
+	fmt.Fprintf(w, string(o))
 }
